7e6558f0e038167f8f7a4608a955f9d4296ec5bc
Fernando Apesteguia <fernando.apesteguia@gmail.com>
2022-09-15 07:43:34 +0200

    Add parser basic functions.
    Basic parser functions to be used as building blocks for higher level
    functionality.  Most of the functions return a boolean specifying if
    they were able to perform the action.

--- internal/parse/parser.go
+++ internal/parse/parser.go
@@ -1,5 +1,7 @@
 package parse
 
+import "strings"
+
 // QueryPart defines a simple interface for all the different parts that
 // compose a ParsedExpr
 type QueryPart interface {
@@ -42,4 +44,67 @@ func (p *Parser) Parse(input string) (*ParsedExpr, error) {
 // [stringPart outputPart stringPart inputPart]
 type ParsedExpr struct {
 	queryParts []QueryPart
+}
+
+// peekByte returns true if the current byte
+// equals the one passed as parameter.
+func (p *Parser) peekByte(b byte) bool {
+	return p.pos < len(p.input) && p.input[p.pos] == b
+}
+
+// skipByte jumps over the current byte if it matches
+// the byte passed as a parameter. Returns true in that case, false otherwise.
+func (p *Parser) skipByte(b byte) bool {
+	if p.pos < len(p.input) && p.input[p.pos] == b {
+		p.pos++
+		return true
+	}
+	return false
+}
+
+// skipByteFind advances the parser until it finds a byte that matches the one
+// passed as parameter and then jumps over it. In that case returns true. If the
+// end of the string is reached and no matching byte was found, it returns
+// false.
+func (p *Parser) skipByteFind(b byte) bool {
+	for i := p.pos; i < len(p.input); i++ {
+		if p.input[i] == b {
+			p.pos = i + 1
+			return true
+		}
+	}
+	return false
+}
+
+// skipSpaces advances the parser jumping over consecutive spaces. It stops when
+// finding a non-space character. Returns true if the parser position was
+// actually changed, false otherwise.
+func (p *Parser) skipSpaces() bool {
+	mark := p.pos
+	for p.pos < len(p.input) {
+		if p.input[p.pos] != ' ' {
+			break
+		}
+		p.pos++
+	}
+	return p.pos != mark
+}
+
+// skipString advances the parser and jumps over the string passed as parameter.
+// In that case returns true, false otherwise.
+// This function is case insensitive.
+func (p *Parser) skipString(s string) bool {
+	if p.pos+len(s) <= len(p.input) &&
+		strings.EqualFold(p.input[p.pos:p.pos+len(s)], s) {
+		p.pos += len(s)
+		return true
+	}
+	return false
+}
+
+// isNameByte returns true if the byte passed as parameter is considered to be
+// one that can be part of a name. It returns false otherwise
+func isNameByte(c byte) bool {
+	return 'A' <= c && c <= 'Z' || 'a' <= c && c <= 'z' ||
+		'0' <= c && c <= '9' || c == '_'
 }
--- internal/parse/parser_test.go
+++ internal/parse/parser_test.go
@@ -12,3 +12,73 @@ func TestInit(t *testing.T) {
 	assert.Equal(t, nil, err)
 	assert.Equal(t, (*ParsedExpr)(nil), expr)
 }
+
+func TestPeekByte(t *testing.T) {
+	p := NewParser()
+	p.Parse("")
+	assert.Equal(t, false, p.peekByte('a'))
+	p.Parse("b")
+	assert.Equal(t, false, p.peekByte('a'))
+	p.Parse("a")
+	assert.Equal(t, true, p.peekByte('a'))
+}
+
+func TestSkipByte(t *testing.T) {
+	p := NewParser()
+	p.Parse("")
+	assert.Equal(t, false, p.skipByte('a'))
+	p.Parse("abc")
+	assert.Equal(t, false, p.skipByte('b'))
+	p.Parse("abc")
+	assert.Equal(t, true, p.skipByte('a'))
+	assert.Equal(t, true, p.peekByte('b'))
+}
+
+func TestSkipByteFind(t *testing.T) {
+	p := NewParser()
+	p.Parse("")
+	assert.Equal(t, false, p.skipByteFind('a'))
+	p.Parse("abcde")
+	assert.Equal(t, false, p.skipByteFind('x'))
+	assert.Equal(t, true, p.skipByteFind('b'))
+	assert.Equal(t, true, p.peekByte('c'))
+	p.Parse("abcde ")
+	assert.Equal(t, true, p.skipByteFind(' '))
+	assert.Equal(t, false, p.skipByteFind(' '))
+}
+
+func TestSkipSpaces(t *testing.T) {
+	p := NewParser()
+	p.Parse("")
+	assert.Equal(t, false, p.skipSpaces())
+	p.Parse("abc    d")
+	assert.Equal(t, false, p.skipSpaces())
+	p.Parse("     abcd")
+	assert.Equal(t, true, p.skipSpaces())
+	assert.Equal(t, true, p.peekByte('a'))
+	p.Parse("  \t  abcd")
+	assert.Equal(t, true, p.skipSpaces())
+	assert.Equal(t, true, p.peekByte('\t'))
+}
+
+func TestSkipString(t *testing.T) {
+	p := NewParser()
+	p.Parse("")
+	assert.Equal(t, false, p.skipString("a"))
+	p.Parse("helloworld")
+	assert.Equal(t, true, p.skipString("hElLo"))
+	assert.Equal(t, true, p.peekByte('w'))
+	p.Parse("hello world")
+	assert.Equal(t, true, p.skipString("hello"))
+	assert.Equal(t, true, p.peekByte(' '))
+}
+
+func TestIsNameByte(t *testing.T) {
+	assert.Equal(t, true, isNameByte('f'))
+	assert.Equal(t, true, isNameByte('0'))
+	assert.Equal(t, true, isNameByte('5'))
+	assert.Equal(t, false, isNameByte('à'))
+	assert.Equal(t, false, isNameByte('\n'))
+	assert.Equal(t, false, isNameByte('\r'))
+	assert.Equal(t, false, isNameByte('\t'))
+}
