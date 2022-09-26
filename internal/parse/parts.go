6040a4bd4f69703609be2bfe17360e95b66848f8
Fernando Apesteguia <fernando.apesteguia@gmail.com>
2022-09-26 10:24:29 +0200

    Address feedback from PR #7
     * Use `FullName` to represent identifiers, either column names or Go
       types.
     * Make the type public.
     * Remove accessors.
     * Eliminate indirection levels.
     * s/passthroughPart/BypassPart
     * Use `p` for all parts when used as receivers.

--- internal/parse/parts.go
+++ internal/parse/parts.go
@@ -1,155 +1,81 @@
 package parse
 
-import "fmt"
-
-// queryPart is an interface to be implemented by all parts.
+// A queryPart represents a section of a parsed SQL statement, which forms
+// a complete query when processed together with its surrounding parts, in
+// their correct order.
 type queryPart interface {
-	// String-like output for debugging purposes
+	// String returns the part's representation for debugging purposes.
 	String() string
 
-	// Translate part to its Sql equivalent
-	ToSql() string
+	// ToSQL returns the SQL representation of the part.
+	ToSQL() string
 }
 
-// qualifiedExpression represents a qualified expression
-// qualified. Examples are:
-//	... column as &Person ... 	<--- "column" is unqualified
-//	... t.column as &Person.name	<--- "name" and "column" and qualified
-type qualifiedExpression struct {
-	qualifier string
-	name      string
+type FullName struct {
+	Prefix, Name string
 }
 
-// InputPart represents an expression used as input in the SDL query.
-// Examples:
-//	$Address
-//	$Address.postal_code
+// InputPart represents a named parameter that will be send to the database
+// while performing the query.
 type InputPart struct {
-	goType
+	FullName
 }
 
 // NewInputPart is a constructor that returns a pointer to an InputPart.
 func NewInputPart(typeName string, tagName string) (*InputPart, error) {
-	if typeName == "" && tagName != "" {
-		return nil, fmt.Errorf("malformed InputPart")
-	}
 	return &InputPart{
-		goType{
-			qualifiedExpression{qualifier: typeName,
-				name: tagName,
-			},
+		FullName{
+			Prefix: typeName,
+			Name:   tagName,
 		},
 	}, nil
 }
 
-func (ip *InputPart) String() string {
+func (p *InputPart) String() string {
 	return ""
 }
 
-func (ip *InputPart) ToSql() string {
+func (p *InputPart) ToSQL() string {
 	return ""
-}
-
-// TypeName returns the name of the type in the input part or empty string if
-// none is present.
-func (ip *InputPart) TypeName() string {
-	return ip.qualifier
-}
-
-// TagName returns the name of the tag in the input part or empty string if
-// none is present.
-func (ip *InputPart) TagName() string {
-	return ip.name
-}
-
-// A column is an internal type that represents the columns selected in the
-// query.
-type column struct {
-	qualifiedExpression
-}
-
-// TableName returns the name of the table that qualifies a column or empty
-// string if there is none. Examples:
-//
-// select t.column	<--- TableName() returns "t"
-// select column	<--- TableName() returns ""
-func (c *column) TableName() string {
-	return c.qualifier
-}
-
-// ColumnName returns the name of the column that is selected in a query or an
-// empty string if there is none. Examples:
-//
-// select t.column	<--- TableName() returns "column"
-func (c *column) ColumnName() string {
-	return c.name
-}
-
-// A goType is an internal type that represents the type part of an output
-// expression in our SDL
-type goType struct {
-	qualifiedExpression
-}
-
-// TypeName returns the name of the type in the output part or empty string if
-// none is present.
-func (gt *goType) TypeName() string {
-	return gt.qualifier
-}
-
-// TagName returns the name of the type in the output part or empty string if
-// none is present.
-func (gt *goType) TagName() string {
-	return gt.name
 }
 
 // OutputPart represents an expression to be used as output in our SDL.
-// Examples:
-// 	&Person
-//	&Person.name
 type OutputPart struct {
-	Column column
-	GoType goType
+	Source FullName
+	Target FullName
 }
 
 // NewOutputPart is a constructor that returns a pointer to an OutputPart.
 func NewOutputPart(tableName string, colName string,
 	typeName string, tagName string) (*OutputPart, error) {
-	if (tableName != "" && colName == "") || (typeName == "" && tagName != "") {
-		return nil, fmt.Errorf("malformed OutputPart")
-	}
 	return &OutputPart{
-		column{
-			qualifiedExpression{qualifier: tableName,
-				name: colName,
-			},
+		FullName{Prefix: tableName,
+			Name: colName,
 		},
-		goType{
-			qualifiedExpression{qualifier: typeName,
-				name: tagName,
-			},
+		FullName{Prefix: typeName,
+			Name: tagName,
 		},
 	}, nil
 }
 
-func (op *OutputPart) String() string {
+func (p *OutputPart) String() string {
 	return ""
 }
 
-func (op *OutputPart) ToSql() string {
+func (p *OutputPart) ToSQL() string {
 	return ""
 }
 
-// passthroughPart represents a part of the SDL that we want to pass to the
+// BypassPart represents a part of the SDL that we want to pass to the
 // backend database verbatim.
-type PassthroughPart struct {
+type BypassPart struct {
 	Chunk string
 }
 
-func (pt *PassthroughPart) String() string {
+func (p *BypassPart) String() string {
 	return ""
 }
 
-func (pt *PassthroughPart) ToSql() string {
+func (p *BypassPart) ToSQL() string {
 	return ""
 }
--- internal/parse/parts_test.go
+++ internal/parse/parts_test.go
@@ -1,7 +1,6 @@
 package parse
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,44 +10,33 @@ func TestInputPart(t *testing.T) {
 	i, err := NewInputPart("mytype", "mytag")
 	assert.Equal(t, nil, err)
 	assert.NotEqual(t, nil, i)
-	assert.Equal(t, "mytype", i.TypeName())
-	assert.Equal(t, "mytag", i.TagName())
-	i, err = NewInputPart("", "mytag")
-	assert.Equal(t, (*InputPart)(nil), i)
-	assert.Equal(t, fmt.Errorf("malformed InputPart"), err)
+	assert.Equal(t, "mytype", i.Prefix)
+	assert.Equal(t, "mytag", i.Name)
 }
 
 func TestOutputPart(t *testing.T) {
 	// Fully specified part
-	i, err := NewOutputPart("mytable", "mycolumn", "mytype", "mytag")
+	p, err := NewOutputPart("mytable", "mycolumn", "mytype", "mytag")
 	assert.Equal(t, nil, err)
-	assert.NotEqual(t, nil, i)
-	assert.Equal(t, "mytable", i.Column.TableName())
-	assert.Equal(t, "mycolumn", i.Column.ColumnName())
-	assert.Equal(t, "mytype", i.GoType.TypeName())
-	assert.Equal(t, "mytag", i.GoType.TagName())
-	// Having table name but not column name is an error
-	i, err = NewOutputPart("mytable", "", "mytype", "mytag")
-	assert.Equal(t, (*OutputPart)(nil), i)
-	assert.Equal(t, fmt.Errorf("malformed OutputPart"), err)
+	assert.NotEqual(t, nil, p)
+	assert.Equal(t, "mytable", p.Source.Prefix)
+	assert.Equal(t, "mycolumn", p.Source.Name)
+	assert.Equal(t, "mytype", p.Target.Prefix)
+	assert.Equal(t, "mytag", p.Target.Name)
 	// Having column name without table is OK
-	i, err = NewOutputPart("", "mycolumn", "mytype", "mytag")
+	p, err = NewOutputPart("", "mycolumn", "mytype", "mytag")
 	assert.Equal(t, nil, err)
-	assert.NotEqual(t, nil, i)
-	assert.Equal(t, "", i.Column.TableName())
-	assert.Equal(t, "mycolumn", i.Column.ColumnName())
-	assert.Equal(t, "mytype", i.GoType.TypeName())
-	assert.Equal(t, "mytag", i.GoType.TagName())
-	// Having tag name but no type name is an error
-	i, err = NewOutputPart("mytag", "mycolumn", "", "mytag")
-	assert.Equal(t, (*OutputPart)(nil), i)
-	assert.Equal(t, fmt.Errorf("malformed OutputPart"), err)
+	assert.NotEqual(t, nil, p)
+	assert.Equal(t, "", p.Source.Prefix)
+	assert.Equal(t, "mycolumn", p.Source.Name)
+	assert.Equal(t, "mytype", p.Target.Prefix)
+	assert.Equal(t, "mytag", p.Target.Name)
 	// Having type name without tag name is OK
-	i, err = NewOutputPart("mytable", "mycolumn", "mytype", "")
+	p, err = NewOutputPart("mytable", "mycolumn", "mytype", "")
 	assert.Equal(t, nil, err)
-	assert.NotEqual(t, nil, i)
-	assert.Equal(t, "mytable", i.Column.TableName())
-	assert.Equal(t, "mycolumn", i.Column.ColumnName())
-	assert.Equal(t, "mytype", i.GoType.TypeName())
-	assert.Equal(t, "", i.GoType.TagName())
+	assert.NotEqual(t, nil, p)
+	assert.Equal(t, "mytable", p.Source.Prefix)
+	assert.Equal(t, "mycolumn", p.Source.Name)
+	assert.Equal(t, "mytype", p.Target.Prefix)
+	assert.Equal(t, "", p.Target.Name)
 }
