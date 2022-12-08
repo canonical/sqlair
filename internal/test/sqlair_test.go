bfcb57aef7c3f6e06bea432f9da777fdf8bc5e91
Alastair Flynn <alastairflynn@hotmail.co.uk>
2022-12-08 15:40:34 +0000

    Make tests work

--- internal/test/sqlair_test.go
+++ internal/test/sqlair_test.go
@@ -8,6 +8,9 @@ import (
 	. "gopkg.in/check.v1"
 )
 
+// Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) { TestingT(t) }
+
 type TestSuite struct{}
 
 var _ = Suite(&TestSuite{})
