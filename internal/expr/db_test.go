5cab63116b038aa7731bc233a51707025d9d64e8
Alastair Flynn <alastairflynn@hotmail.co.uk>
2023-03-08 15:21:55 +0000

    Add DB to export_test.go

--- internal/expr/db_test.go
+++ internal/expr/db_test.go
@@ -150,7 +150,7 @@ func (s *DBSuite) TestValidDecode(c *C) {
 		c.Fatal(err)
 	}
 
-	sqlairDB := expr.NewDB(db)
+	sqlairDB := expr.NewTestDB(db)
 
 	parser := expr.NewParser()
 
@@ -246,7 +246,7 @@ func (s *DBSuite) TestDecodeErrors(c *C) {
 		c.Fatal(err)
 	}
 
-	sqlairDB := expr.NewDB(db)
+	sqlairDB := expr.NewTestDB(db)
 
 	parser := expr.NewParser()
 
@@ -330,7 +330,7 @@ func (s *DBSuite) TestValidAll(c *C) {
 		c.Fatal(err)
 	}
 
-	sqlairDB := expr.NewDB(db)
+	sqlairDB := expr.NewTestDB(db)
 
 	parser := expr.NewParser()
 
@@ -405,7 +405,7 @@ func (s *DBSuite) TestValidOne(c *C) {
 		c.Fatal(err)
 	}
 
-	sqlairDB := expr.NewDB(db)
+	sqlairDB := expr.NewTestDB(db)
 
 	parser := expr.NewParser()
 
@@ -469,7 +469,7 @@ func (s *DBSuite) TestOneErrors(c *C) {
 		c.Fatal(err)
 	}
 
-	sqlairDB := expr.NewDB(db)
+	sqlairDB := expr.NewTestDB(db)
 
 	parser := expr.NewParser()
 
@@ -498,123 +498,6 @@ func (s *DBSuite) TestOneErrors(c *C) {
 		err = resultExpr.One(t.outputs...)
 		c.Assert(err, ErrorMatches, t.err,
 			Commentf("test %q failed:\ninput: %s\noutputs: %s", t.summery, t.query, t.outputs))
-	}
-
-	_, err = db.Exec(drop)
-	if err != nil {
-		c.Fatal(err)
-	}
-}
-
-type JujuLeaseKey struct {
-	Namespace string `db:"type"`
-	ModelUUID string `db:"model_uuid"`
-	Lease     string `db:"name"`
-}
-
-type JujuLeaseInfo struct {
-	Holder string `db:"holder"`
-	Expiry int    `db:"expiry"`
-}
-
-func JujuStoreLeaseDB() (string, *sql.DB, error) {
-	create := `
-CREATE TABLE lease (
-	model_uuid text,
-	name text,
-	holder text,
-	expiry integer,
-	lease_type_id text
-);
-CREATE TABLE lease_type (
-	id text,
-	type text
-);
-
-`
-	drop := `
-	 drop table lease;
-	 drop table lease_type;
-	 `
-
-	inserts := []string{
-		"INSERT INTO lease VALUES ('uuid1', 'name1', 'holder1', 1, 'type_id1');",
-		"INSERT INTO lease VALUES ('uuid2', 'name2', 'holder2', 4, 'type_id1');",
-		"INSERT INTO lease VALUES ('uuid3', 'name3', 'holder3', 7, 'type_id2');",
-		"INSERT INTO lease_type VALUES ('type_id1', 'type1');",
-		"INSERT INTO lease_type VALUES ('type_id2', 'type2');",
-	}
-
-	db, err := createExampleDB(create, inserts)
-	if err != nil {
-		return "", nil, err
-	}
-	return drop, db, nil
-
-}
-
-func (s *DBSuite) TestJujuStore(c *C) {
-	var tests = []struct {
-		summery  string
-		query    string
-		types    []any
-		inputs   []any
-		expected [][]any
-	}{{
-		summery: "juju store lease group query",
-		query: `
-SELECT (t.type, l.model_uuid, l.name) AS &JujuLeaseKey.*, (l.holder, l.expiry) AS &JujuLeaseInfo.*
-FROM   lease l JOIN lease_type t ON l.lease_type_id = t.id
-WHERE  t.type = $JujuLeaseKey.type
-AND    l.model_uuid = $JujuLeaseKey.model_uuid`,
-		types:    []any{JujuLeaseKey{}, JujuLeaseInfo{}},
-		inputs:   []any{JujuLeaseKey{Namespace: "type1", ModelUUID: "uuid1"}},
-		expected: [][]any{{JujuLeaseKey{Namespace: "type1", ModelUUID: "uuid1", Lease: "name1"}, JujuLeaseInfo{Holder: "holder1", Expiry: 1}}},
-	}}
-
-	drop, db, err := JujuStoreLeaseDB()
-	if err != nil {
-		c.Fatal(err)
-	}
-
-	sqlairDB := expr.NewDB(db)
-
-	parser := expr.NewParser()
-
-	for _, t := range tests {
-		parsedExpr, err := parser.Parse(t.query)
-		if err != nil {
-			c.Errorf("test %q failed (Parse):\ninput: %s\nerr: %s\n", t.summery, t.query, err)
-			continue
-		}
-		preparedExpr, err := parsedExpr.Prepare(t.types...)
-		if err != nil {
-			c.Errorf("test %q failed (Prepare):\ninput: %s\nerr: %s\n", t.summery, t.query, err)
-			continue
-		}
-		completedExpr, err := preparedExpr.Complete(t.inputs...)
-		if err != nil {
-			c.Errorf("test %q failed (Complete):\ninput: %s\nerr: %s\n", t.summery, t.query, err)
-			continue
-		}
-		resultExpr, err := sqlairDB.Query(completedExpr)
-		if err != nil {
-			c.Errorf("test %q failed (Exec):\ninput: %s\nerr: %s\n", t.summery, t.query, err)
-			continue
-		}
-		res, err := resultExpr.All()
-		if err != nil {
-			c.Errorf("test %q failed (All):\ninput: %s\nerr: %s\n", t.summery, t.query, err)
-			continue
-		}
-		c.Assert(res, HasLen, len(t.expected),
-			Commentf("test %q failed:\ninput: %s", t.summery, t.query))
-		for i, es := range t.expected {
-			for j, e := range es {
-				c.Assert(res[i][j], DeepEquals, e,
-					Commentf("test %q failed:\ninput: %s", t.summery, t.query))
-			}
-		}
 	}
 
 	_, err = db.Exec(drop)
