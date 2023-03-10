5f3bc311e2a13ffb4225558fd49636dc4635f471
Alastair Flynn <alastairflynn@hotmail.co.uk>
2023-03-10 13:45:23 +0000

    Edit newline

--- internal/expr/complete.go
+++ internal/expr/complete.go
@@ -60,6 +60,7 @@ func (pe *PreparedExpr) Complete(args ...any) (ce *CompletedExpr, err error) {
 
 			return nil, fmt.Errorf("%s not referenced in query", t.Name())
 		}
+
 	}
 
 	// Query parameteres.
