8303ff7b1867c9aa810fb4f73a3e98111c9a7819
Alastair Flynn <alastairflynn@hotmail.co.uk>
2023-03-16 09:18:50 +0000

    Remove TODO

--- internal/expr/prepare.go
+++ internal/expr/prepare.go
@@ -185,7 +185,7 @@ func (pe *ParsedExpr) Prepare(args ...any) (expr *PreparedExpr, err error) {
 			if k == reflect.Pointer {
 				return nil, fmt.Errorf("need struct, got pointer. Prepare takes structs by value as they are only used for their type information")
 			}
-			return nil, fmt.Errorf("need struct, got %s", k) // TODO Add test for this
+			return nil, fmt.Errorf("need struct, got %s", k)
 		}
 		info, err := typeInfo(arg)
 		if err != nil {
