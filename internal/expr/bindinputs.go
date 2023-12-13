24640e283a5a9043650591dd779bf1df57a4c99f
Alastair Flynn <af@aflynn.uk>
2023-12-13 13:56:25 +0000

    Add default case to in BindInputs

--- internal/expr/bindinputs.go
+++ internal/expr/bindinputs.go
@@ -76,6 +76,8 @@ func (tbe *TypeBoundExpr) BindInputs(args ...any) (pq *PrimedQuery, err error) {
 			}
 		case *bypass:
 			sqlStr.WriteString(te.chunk)
+		default:
+			return nil, fmt.Errorf("internal error: unknown type %T", te)
 		}
 	}
 
