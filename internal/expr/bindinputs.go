536181adfad2ef3add6e3d4ed71ff74344fffabc
Alastair Flynn <af@aflynn.uk>
2023-12-14 09:23:37 +0000

    Change formatter in default and add internal error to message

--- internal/expr/bindinputs.go
+++ internal/expr/bindinputs.go
@@ -77,7 +77,7 @@ func (tbe *TypeBoundExpr) BindInputs(args ...any) (pq *PrimedQuery, err error) {
 		case *bypass:
 			sqlStr.WriteString(te.chunk)
 		default:
-			return nil, fmt.Errorf("invalid expression type %t", te)
+			return nil, fmt.Errorf("internal error: unknown expression type %T", te)
 		}
 	}
 
